f0983102ed5a3ca9544fe4a7423808580fae2925
isso0424 <isso04240129@gmail.com>
2020-11-25 11:20:14 +0900

    feat: verifier usecase

---
+++ src/domain/token.ts
@@ -0,0 +1,6 @@
+import { Dayjs } from "dayjs";
+
+export interface AccessToken {
+  token: string;
+  issuanceTime: Dayjs;
+}
---
+++ src/usecase/verifier.ts
@@ -0,0 +1,5 @@
+import {AccessToken} from "../domain/token";
+
+export interface Verifier {
+  getAccessToken(): AccessToken;
+}

d2ff1ff1e3d2445bd922a72a2fd0450efbb02281
isso0424 <isso04240129@gmail.com>
2020-11-26 12:17:53 +0900

    feat: GET Requester

--- src/client/requester.ts
+++ src/client/requester.ts
@@ -1,6 +1,30 @@
+import axios from "axios";
 const baseURL = "https://api.spotify.com/v1";
 
 export class Requester {
-  async request(endpoint: string): Promise<Record<string, unknown>> {
+  async getData(
+    endpoint: string,
+    queries: Record<string, string>,
+  ): Promise<Record<string, unknown>> {
+    const urlForEndpoint = baseURL + endpoint;
+    const requestURL = Requester.embedQueriesToURL(urlForEndpoint, queries);
+
+    const response = await axios.get(requestURL);
+    if (response.status >= 200 && response.status < 300) {
+      throw "Request is failed with status code " + response.status;
+    }
+
+    return response.data;
+  }
+  
+  private static embedQueriesToURL(url: string, queries: Record<string, string>): string {
+    if (Object.entries(queries).length == 0)
+      return url
+    
+    Object.entries(queries).forEach(([key, value], index) => {
+      url += `${index == 0 ? "?" : "&"}${key}=${value}`;
+    });
+
+    return url
   }
 }

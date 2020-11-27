dad7ad7c891c2e92f72401a5747982321b8d9086
isso0424 <isso04240129@gmail.com>
2020-11-27 21:07:54 +0900

    fix: verifier support new handler

--- src/client/verifier.ts
+++ src/client/verifier.ts
@@ -3,38 +3,15 @@ import dayjs from "dayjs";
 import { AccessToken } from "../domain/token";
 import { IVerifier } from "../usecase/verifier";
 
-const refreshTokenURL = "https://accounts.spotify.com/api/token";
-
 export class Verifier implements IVerifier {
-  constructor(refreshToken: string, clientID: string) {
-    this.refreshToken = refreshToken;
-    this.clientID = clientID;
-  }
-  
-  refreshToken: string;
-
-  clientID: string;
-
   async getAccessToken(): Promise<AccessToken> {
     const createdTime = dayjs();
-    const response = await axios.post(
-      refreshTokenURL, {
-        grant_type: "refresh_token",
-        refresh_token: this.refreshToken,
-        client_id: this.clientID,
-      },
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }
-    );
-
+    const response = await axios.get("http://localhost:8000/token")
     if (response.status != 200) {
       throw "Error occurred in get token with status code " + response.status;
     }
 
-    const accessToken = response.data["access_token"];
+    const accessToken = response.data;
 
     return {
       token: accessToken,

0e721440bb0233b2ff815a423b9e03ac980bcdb0
isso0424 <isso04240129@gmail.com>
2020-11-27 10:56:04 +0900

    fix: token handler

--- auth/refresher/handler.go
+++ auth/refresher/handler.go
@@ -3,8 +3,8 @@ package refresher
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -22,41 +22,48 @@ func getAccessTokenHandler(writer http.ResponseWriter, request *http.Request) {
 		strings.NewReader(body.Encode()),
 	)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
 	idSet := []byte(ids.Client + ":" + ids.Secret)
 	encodedID := base64.StdEncoding.EncodeToString(idSet)
 	authorization := "Basic " + encodedID
 
 	req.Header.Set("Authorization", authorization)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	client := &http.Client{}
 	response, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		writer.Write([]byte(err.Error()))
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	defer response.Body.Close()
 
 	responseBuffer, _ := ioutil.ReadAll(response.Body)
-	responseBody := map[string]string{}
+	log.Println(string(responseBuffer))
+	responseBody := refreshTokenResponse{}
 	err = json.Unmarshal(responseBuffer, &responseBody)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if responseBody.Error != "" {
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	accessToken := responseBody["access_token"]
+	accessToken := responseBody.AccessToken
+	log.Println(accessToken)
 
 	buffer := []byte(accessToken)
 	_, err = writer.Write(buffer)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -68,10 +75,18 @@ func getClientIDHandler(writer http.ResponseWriter, request *http.Request) {
 
 	_, err := writer.Write(buffer)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	writer.WriteHeader(http.StatusOK)
+}
+
+type refreshTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+	ExpiresIn   int32  `json:"expires_in"`
+	Error       string `json:"error"`
 }

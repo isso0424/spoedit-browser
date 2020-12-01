0c89500d784a20effbf5c2aecbaf55af5a343794
isso0424 <isso04240129@gmail.com>
2020-12-01 23:32:47 +0900

    feat: define album interface

---
+++ src/domain/album.ts
@@ -0,0 +1,15 @@
+import {Track} from "./track";
+
+export interface Album {
+   name: string;
+
+   tracks: Array<Track>;
+
+   artistName: string;
+
+   id: string;
+
+   uri: string;
+
+   imageURL: string;
+}

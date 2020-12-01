c0f37c0639e8e09014a8f73be0b3c2ade11a8961
isso0424 <isso04240129@gmail.com>
2020-12-01 19:05:10 +0900

    feat: playlist deleter

---
+++ src/client/playlist/deleter.ts
@@ -0,0 +1,31 @@
+import {Playlist} from "../../domain/playlist";
+import {IPlaylistDeleter} from "../../usecase/deleter";
+import {IRequester} from "../../usecase/requester";
+import {IVerifier} from "../../usecase/verifier";
+
+const endpointTemplate = "playlists/id/followers"
+
+export class PlaylistDeleter implements IPlaylistDeleter {
+  constructor(verifier: IVerifier, requester: IRequester) {
+    this.verifier = verifier;
+    this.requester = requester;
+  }
+
+  private verifier: IVerifier;
+
+  private requester: IRequester;
+
+  async deletePlaylist(playlist: Playlist): Promise<Playlist> {
+    const accessToken = await this.verifier.getAccessToken();
+    const endpoint = endpointTemplate.replace("id", playlist.id);
+
+    await this.requester.delete(
+      endpoint,
+      undefined,
+      undefined,
+      { Authorization: "Bearer " + accessToken.token },
+    );
+
+    return playlist;
+  }
+}

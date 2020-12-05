0b4c2835a5af8580f27d795063a76c977488dd40
isso0424 <isso04240129@gmail.com>
2020-12-05 20:54:56 +0900

    delete: SelectedCard

--- src/components/card/selected.tsx
+++
@@ -1,9 +0,0 @@
-import {Card, useTheme, withStyles} from "@material-ui/core";
-
-const theme = useTheme();
-
-export const SelectedCard = withStyles({
-  root: {
-    color: theme.palette.primary.main,
-  }
-})(Card);
